603bb26a0a80ac2e9512ad21e692207ffceec97f
Summer T <summer@summertime-development.com>
2023-04-01 12:05:37 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+    consoleBuffer,
+    consoleEnd,
+    consoleStart,
+    validateFxn,
+} from './helpers.js';
+
+
+consoleStart();
+
+// validateFxn(bf.contains('Brian'), true);
+
+
+consoleEnd();
+consoleBuffer();
+
+export {};
