45592fe0e87882764c6cd9f6f190f8f1c21eff1f
Summer T <summer@summertime-development.com>
2023-04-01 21:19:38 -0500

    starting

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,28 @@ import {
     validateFxn,
 } from './helpers.js';
 
+function minimumBribes(peopleArray: number[]): number | string {
+    const visited = [];
+
+    let bribeCount = 0;
+    for (let i = 0; i < peopleArray.length; i++) {
+        if (
+            i < peopleArray.length + 1 &&
+            peopleArray[i] === peopleArray[i] + 1
+        ) {
+            //skip
+        } else {
+            bribeCount++;
+        }
+    }
+
+    return 0;
+}
 
 consoleStart();
 
-// validateFxn(bf.contains('Brian'), true);
-
+validateFxn(minimumBribes([2, 1, 5, 3, 4]), 3);
+validateFxn(minimumBribes([2, 5, 1, 3, 4]), 'Too chaotic');
 
 consoleEnd();
 consoleBuffer();
